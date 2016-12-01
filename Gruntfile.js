9dd00151dc6472bec8039d24d5d87ec37f675b78
vchaillou <valentin.chaillou@laposte.net>
2016-12-01 08:57:28 +0100

    BUGFIX port
    Ajout du port de Heroku

--- Gruntfile.js
+++ Gruntfile.js
@@ -70,9 +70,9 @@ module.exports = function (grunt) {
     // The actual grunt server settings
     connect: {
       options: {
-        port: 9000,
+        port: process.env.PORT || 9000,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost',
+        hostname: '0.0.0.0',
         livereload: 35729
       },
       livereload: {
@@ -220,7 +220,7 @@ module.exports = function (grunt) {
             }
           }
       }
-    }, 
+    },
 
     // Renames files for browser caching purposes
     filerev: {
